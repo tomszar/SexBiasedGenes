15dec644abec99413bf9574d30ee00feec6b6730
tomszar <tomasgzarzar@gmail.com>
2018-01-27 11:29:49 -0500

    script to crop plink files

---
+++ Code/2018-01-GWASGeneRangePrep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#First move to the database folder where imputed genotypes are
+cd ../DataBases/ImputedGenotypes
+mkdir croppedfiles
+
+#Then, for each plink file, cropp the chromosome file using the GeneRanges
+generanges="../../Results/WGCNA/Adipose/GeneRanges.txt"
+for file in *.bed
+do
+	inputname=$(echo $file | cut -f 1 -d '.' | tr '/' '\\')
+	echo $inputname
+	counter="${inputname##*_}"
+	echo $counter
+	outname="cropped_chr${counter}"
+	./plink --bfile $inputname --extract range $(echo $generanges) --make-bed --out $outname
+done
+
+mv cropped*.* croppedfiles/
